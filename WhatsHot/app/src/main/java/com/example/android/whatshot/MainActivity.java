6d1a11e17d38b256c86266aff9f631f2fc280151
Hammed opejin <hammedopejin@Hammeds-MacBook-Pro.local>
2018-03-22 21:23:59 -0700

    Adapter class and recycler view prep, waiting on URI to access Content Provider and details of variables to be expeted.

--- WhatsHot/app/src/main/java/com/example/android/whatshot/MainActivity.java
+++ WhatsHot/app/src/main/java/com/example/android/whatshot/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,7 +14,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    // Member variables for the adapter and RecyclerView
+    private PopularTimesAdapter mAdapter;
+    RecyclerView mRecyclerView;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,7 +32,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "HammedKenda branch :)", Toast.LENGTH_LONG);
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new PopularTimesAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
--- WhatsHot/app/src/main/java/com/example/android/whatshot/PopularTimesAdapter.java
+++ WhatsHot/app/src/main/java/com/example/android/whatshot/PopularTimesAdapter.java
@@ -1,8 +1,12 @@
 package com.example.android.whatshot;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Pedram on 3/18/2018.
@@ -12,9 +16,28 @@ import android.view.ViewGroup;
 
 public class PopularTimesAdapter extends RecyclerView.Adapter<PopularTimesAdapter.PopularTimesAdapterViewHolder> {
 
+    // Class variable that holds  the Context
+    private Context mContext;
+
+
+    /**
+     * Constructor for the CustomCursorAdapter that initializes the Context.
+     *
+     * @param mContext the current Context
+     */
+    public PopularTimesAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
     @Override
     public PopularTimesAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+
+            // Inflate the main_list_item to a view
+            View view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.main_list_item, parent, false);
+
+            return new PopularTimesAdapterViewHolder(view);
     }
 
     @Override
@@ -29,8 +52,13 @@ public class PopularTimesAdapter extends RecyclerView.Adapter<PopularTimesAdapte
 
     class PopularTimesAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        TextView establishmentNameView;
+        TextView rankView;
+
         public PopularTimesAdapterViewHolder(View itemView) {
             super(itemView);
+            establishmentNameView = itemView.findViewById(R.id.establishment_name);
+            rankView = itemView.findViewById(R.id.rankTextView);
         }
 
         @Override
