1a9a166138ca571f6924dd5ea81aac131616f111
Yanilda <yanilda.peralta@gmail.com>
2018-04-01 17:03:14 -0400

    Created some textviews for the detail layout and linked one textview

--- WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
+++ WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
@@ -2,13 +2,16 @@ package com.example.android.whatshot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 // TODO: Yany, Kerry
 public class DetailActivity extends AppCompatActivity {
-
+    private TextView mLocationNameTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        mLocationNameTV=(TextView)findViewById(R.id.location_name);
+
     }
 }
