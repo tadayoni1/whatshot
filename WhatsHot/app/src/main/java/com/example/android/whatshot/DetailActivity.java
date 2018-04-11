33dcb2997b6502007ce70d6c961d068d25299374
Yanilda <yanilda.peralta@gmail.com>
2018-04-11 01:17:49 -0400

    Edited the  detail activity layout and the Detail activity class.

--- WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
+++ WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
@@ -7,44 +7,23 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.example.android.whatshot.data.PopularTimesContract;
-
 // TODO: Yany, Kerry
 public class DetailActivity extends AppCompatActivity implements  LoaderManager.LoaderCallbacks<Cursor> {
-    private TextView mLocationNameTV;
-    private TextView mDateTV;
-    private RatingBar mRankingRB;
+    private TextView mLocationNameTV, mDateTV, mPopularityTV, mAddressTV,mPhoneTV;
+
+    private RatingBar mRatingRB;
 
 
-    public static final String[] POPULARTIMES_DETAIL_PROJECTION = {
-            PopularTimesContract.VenueEntry.COLUMN_NAME,
-            PopularTimesContract.VenueEntry.COLUMN_ADDRESS,
-            PopularTimesContract.VenueEntry.COLUMN_RATING,
-            PopularTimesContract.VenueEntry.COLUMN_INTERNATIONAL_PHONE_NUMBER,
-            PopularTimesContract.VenueEntry.COLUMN_TYPES,
-            PopularTimesContract.VenueEntry.COLUMN_LAT,
-            PopularTimesContract.VenueEntry.COLUMN_LNG,
-            PopularTimesContract.VenueEntry.VenueHoursEntry.COLUMN_DAY,
-            PopularTimesContract.VenueEntry.VenueHoursEntry.COLUMN_HOUR,
-            PopularTimesContract.VenueEntry.VenueHoursEntry.COLUMN_POPULARITY,
-            PopularTimesContract.VenueEntry.COLUMN_VENUE_ID
-
-    };
-    public static final int INDEX_VENUE_NAME=0;
-    public static final int INDEX_VENUE_ADDRESS=1;
-    public static final int INDEX_VENUE_RATING=2;
-    public static final int INDEX_VENUE_PHONE=3;
-    public static final int INDEX_VENUE_TYPE=4;
-    public static final int INDEX_VENUE_LAT=5;
-    public static final int INDEX_VENUE_LNG=6;
-    public static final int INDEX_VENUE_DAY=7;
-    public static final int INDEX_VENUE_HOUR=8;
-    public static final int INDEX_VENUE_POPULARITY=9;
-    public static final int INDEX_VENUE_ID=10;
+    public static final int INDEX_VENUE_NAME=2;
+    public static final int INDEX_VENUE_ADDRESS=3;
+    public static final int INDEX_VENUE_PHONE=4;
+    public static final int INDEX_VENUE_RATING=5;
+    public static final int INDEX_VENUE_LAT=7;
+    public static final int INDEX_VENUE_LNG=8;
+    public static final int INDEX_VENUE_TYPE=9;
 
 
     private static final int ID_DETAIL_LOADER = 447;
@@ -55,15 +34,16 @@ public class DetailActivity extends AppCompatActivity implements  LoaderManager.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        mLocationNameTV=(TextView)findViewById(R.id.location_name);
+        mLocationNameTV=(TextView)findViewById(R.id.tv_location_name);
+        mPhoneTV=(TextView)findViewById(R.id.tv_phone);
+        mAddressTV=(TextView)findViewById(R.id.tv_address);
+
+        mPopularityTV=(TextView)findViewById(R.id.popularity);
         mDateTV=(TextView)findViewById(R.id.date);
-        mRankingRB = (RatingBar)findViewById(R.id.ratings);
+        mRatingRB = (RatingBar)findViewById(R.id.ratings);
 
        mUri = getIntent().getData();
 
-        Bundle bundle=getIntent().getExtras();
-
-        Log.d("detail activity", "Arrived Detail Activity"+mUri.getPath());
         if (mUri == null) throw new NullPointerException("URI for DetailActivity cannot be null");
         getSupportLoaderManager().initLoader(ID_DETAIL_LOADER,null, this);
     }
@@ -100,11 +80,20 @@ public class DetailActivity extends AppCompatActivity implements  LoaderManager.
             return;
         }
 
-        //String venueName=data.getString(INDEX_VENUE_NAME);
-       // mLocationNameTV.setText(venueName);
+        String venueName=data.getString(INDEX_VENUE_NAME);
+        mLocationNameTV.setText(venueName);
 
-        //String rating= data.getString(INDEX_VENUE_RATING);
- //       Log.i("Testing rating", rating);
+        String venuePhone="Phone:\n"+data.getString(INDEX_VENUE_PHONE);
+        mPhoneTV.setText(venuePhone);
+
+        String venueAddress="Address:\n"+data.getString(INDEX_VENUE_ADDRESS);
+        mAddressTV.setText(venueAddress);
+
+        String[] venueType=data.getString(INDEX_VENUE_TYPE).split(",");
+        mPopularityTV.setText("Type:\n"+venueType[0].replaceAll("[^a-zA-Z0-9]",""));
+
+        float rating= Float.parseFloat(data.getString(INDEX_VENUE_RATING));
+        mRatingRB.setRating(rating);
 
 
     }
