6a2c8a2ef127ecfa877d7069063394995dbe7096
Yanilda <yanilda.peralta@gmail.com>
2018-04-12 11:41:56 -0400

    populated table, added a textview

--- WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
+++ WhatsHot/app/src/main/java/com/example/android/whatshot/DetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -14,7 +15,8 @@ import com.example.android.whatshot.data.PopularTimesContract;
 
 // TODO: Yany, Kerry
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    private TextView mLocationNameTV, mTypeTV, mPopularityTV, mAddressTV, mPhoneTV, mMonTV;
+    private TextView mLocationNameTV, mTypeTV, mPopularityTV, mAddressTV, mPhoneTV, mRatingCount;
+    private TextView mMonTV, mTueTV, mWedTV,mThuTV,mFriTV,mSatTV,mSunTV;
 
     private RatingBar mRatingRB;
 
@@ -23,7 +25,7 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     public static final int INDEX_VENUE_ADDRESS = 3;
     public static final int INDEX_VENUE_PHONE = 4;
     public static final int INDEX_VENUE_RATING = 5;
-
+    public static final int INDEX_VENUE_RATING_COUNT = 6;
     public static final int INDEX_VENUE_LAT = 7;
     public static final int INDEX_VENUE_LNG = 8;
     public static final int INDEX_VENUE_TYPE = 9;
@@ -43,10 +45,18 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         mPhoneTV = (TextView) findViewById(R.id.tv_phone);
         mAddressTV = (TextView) findViewById(R.id.tv_address);
         mTypeTV = (TextView) findViewById(R.id.tv_type);
+        mPopularityTV = (TextView) findViewById(R.id.tv_popularity);
+        mRatingCount=(TextView)findViewById(R.id.tv_rating_count);
+        mRatingRB = (RatingBar) findViewById(R.id.rb_rating);
 
-        mPopularityTV = (TextView) findViewById(R.id.popularity);
-        mRatingRB = (RatingBar) findViewById(R.id.ratings);
         mMonTV = (TextView) findViewById(R.id.tv_monday);
+        mTueTV = (TextView) findViewById(R.id.tv_tuesday);
+        mWedTV = (TextView) findViewById(R.id.tv_wednesday);
+        mThuTV = (TextView) findViewById(R.id.tv_thursday);
+        mFriTV = (TextView) findViewById(R.id.tv_friday);
+        mSatTV = (TextView) findViewById(R.id.tv_saturday);
+        mSunTV = (TextView) findViewById(R.id.tv_sunday);
+
 
         mVenueUri = Uri.parse(getIntent().getStringExtra(MainActivity.DETAIL_ACTIVITY_EXTRA_VENUE));
         mVenueDetailUri = Uri.parse(getIntent().getStringExtra(MainActivity.DETAIL_ACTIVITY_EXTRA_VENUE_DETAILS));
@@ -112,22 +122,64 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
                 String venueType = "Type: " + venueTypes[0].replaceAll("[^a-zA-Z0-9]", "");
                 mTypeTV.setText(venueType);
 
-                String popularity = data.getString(data.getColumnIndex(PopularTimesContract.VenueEntry.COLUMN_RATING_N)) + " users rated this venue.";
-                mPopularityTV.setText(popularity);
+                String todayPopularity=data.getString(data.getColumnIndex(PopularTimesContract.VenueEntry.VenueHoursEntry.COLUMN_POPULARITY));
+                mPopularityTV.setText(todayPopularity);
+
+                String ratingCount = data.getString(INDEX_VENUE_RATING_COUNT) + " users rated this venue.";
+                mRatingCount.setText(ratingCount);
 
                 float rating = Float.parseFloat(data.getString(INDEX_VENUE_RATING));
                 mRatingRB.setRating(rating);
-                mMonTV.setText(data.getString(data.getColumnIndex(PopularTimesContract.VenueEntry.VenueHoursEntry.COLUMN_POPULARITY)));
+
                 break;
             case ID_VENUE_DETAIL_LOADER:
-                // TODO: Yany, fill popularity table
-                /* data is a cursor with 168 rows each row has venue_id, day, hour and popularity
-                 * data.getString(0) returns venue_id
-                 * data.getString(1) returns day
-                 * data.getString(2) returns hour
-                 * data.getString(3) returns popularity
-                 * you may add all 24 popularity numbers for each day to calculate popularity for each day
-                 */
+
+                if(data==null){
+                    Log.d("OtherThings","No data");
+                    return;
+                }
+
+                int monTotal=0;
+                int tueTotal=0;
+                int wedTotal=0;
+                int thuTotal=0;
+                int friTotal=0;
+                int satTotal=0;
+                int sunTotal=0;
+
+
+                while (data.moveToNext()) {
+                    if(data.getString(2).equals("0")){
+                        monTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("1")){
+                        tueTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("2")){
+                        wedTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("3")){
+                        thuTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("4")){
+                        friTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("5")){
+                        satTotal+=Integer.valueOf(data.getString(4));
+                    }
+                    if(data.getString(2).equals("6")) {
+                        sunTotal += Integer.valueOf(data.getString(4));
+                    }
+                }
+                mMonTV.setText(String.valueOf(monTotal));
+                mTueTV.setText(String.valueOf(tueTotal));
+                mWedTV.setText(String.valueOf(wedTotal));
+                mThuTV.setText(String.valueOf(thuTotal));
+                mFriTV.setText(String.valueOf(friTotal));
+                mSatTV.setText(String.valueOf(satTotal));
+                mSunTV.setText(String.valueOf(sunTotal));
+
+
                 break;
         }
 
